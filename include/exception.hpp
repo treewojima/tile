73f26d170a4e68fdb77d8cf5df53bfbad66a9121
Scott Bishop <treewojima@gmail.com>
2015-10-20 00:26:52 -0400

    implemented basic state machine; refactored specific exceptions out of exception.hpp header

--- include/exception.hpp
+++ include/exception.hpp
@@ -32,24 +32,13 @@
 class Exception : public std::runtime_error
 {
 public:
-    using std::runtime_error::runtime_error;
+	using std::runtime_error::runtime_error;
 };
 
-class SDLException : public Exception
-{
-public:
-    SDLException(const std::string &msg) : Exception(msg) {}
-    SDLException() : Exception(SDL_GetError()) {}
-};
 
-class SDLImageException : public SDLException
-{
-public:
-    SDLImageException(const std::string &msg) : SDLException(msg) {}
-    SDLImageException() : SDLException(IMG_GetError()) {}
-};
 
 #ifdef _ENABLE_AUDIO
+#error move this class to another file
 class SDLMixerException : public SDLException
 {
 public:
@@ -58,40 +47,8 @@ public:
 };
 #endif
 
-class GLException : public Exception
-{
-public:
-    GLException(const std::string &msg) : Exception(msg) {}
-    explicit GLException(GLenum error) : Exception(getErrorString(error)) {}
 
-private:
-    std::string getErrorString(GLenum error)
-    {
-        std::ostringstream ss;
-        ss << gluErrorString(error);
-        return ss.str();
-    }
-};
 
-class GLEWException : public GLException
-{
-public:
-    GLEWException(const std::string &msg) : GLException(msg) {}
-    explicit GLEWException(GLenum error) : GLException(getErrorString(error)) {}
 
-private:
-    std::string getErrorString(GLenum error)
-    {
-        std::ostringstream ss;
-        ss << glewGetErrorString(error);
-        return ss.str();
-    }
-};
-
-class MapException : public Exception
-{
-public:
-    using Exception::Exception;
-};
 
 #endif
--- include/map.hpp
+++ include/map.hpp
@@ -27,6 +27,7 @@
 #include <tmx/Map.h>
 
 #include "entity.hpp"
+#include "exception.hpp"
 #include "texture.hpp"
 
 class Map
@@ -58,6 +59,12 @@ private:
     TextureManager::ResourcePtr _renderedMapTexture;
 
     void loadTilesetTextures();
+};
+
+class MapException : public Exception
+{
+public:
+	using Exception::Exception;
 };
 
 #endif
