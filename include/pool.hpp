3f1cd61e3648a8c2d84b31240c148405f2f3c241
Scott Bishop <treewojima@gmail.com>
2016-06-25 13:28:10 -0400

    updated pool a bit with a simple inheritable "PoolableObject" type

--- include/pool.hpp
+++ include/pool.hpp
@@ -4,15 +4,16 @@
 #include <cassert>
 #include <type_traits>
 
-template <class T>
+// Compile-time-sized pool class
+template <class T, unsigned N>
 class Pool
 {
 public:
-    Pool(unsigned capacity);
+    Pool();
     ~Pool();
 
-    template <class U = T, class... Args>
-    U *allocate(Args&& ... args);
+    template <class U = T>
+    U *allocate();
 
     void release(T *object) noexcept;
 
@@ -23,42 +24,71 @@ private:
         PoolObject *next;
     };
 
-    unsigned _capacity;
     unsigned char *_rawPool;
     PoolObject *_pool;
     PoolObject *_freeListHead;
 };
 
-template <class T>
-Pool<T>::Pool(unsigned capacity) :
-    _capacity(capacity),
+// Poolable object that overrides operators new and delete
+template <class T, unsigned N>
+class PoolableObject
+{
+public:
+    static void *operator new(std::size_t sz)
+    {
+        void *p = getPool().allocate();
+        std::cout << "allocated object at " << p << std::endl;
+        return p;
+    }
+
+    static void operator delete(void *p)
+    {
+        if (p)
+        {
+            std::cout << "releasing object at " << p << std::endl;
+            getPool().release(static_cast<T *>(p));
+        }
+    }
+
+protected:
+    static Pool<T, N> &getPool()
+    {
+        static Pool<T, N> pool;
+        return pool;
+    }
+};
+
+template <class T, unsigned N>
+Pool<T, N>::Pool() :
+    _rawPool(nullptr),
     _pool(nullptr),
     _freeListHead(nullptr)
 {
-    assert(capacity > 0);
+    static_assert(N > 0,
+                  "pool capacity must be greater than zero");
 
     // Allocate a raw chunk of bytes for memory
-    _rawPool = new unsigned char[_capacity * sizeof(PoolObject)];
+    _rawPool = new unsigned char[N * sizeof(PoolObject)];
     _pool = reinterpret_cast<PoolObject *>(_rawPool);
 
     // Initialize the free list
     _freeListHead = _pool;
-    for (unsigned i = 0; i < _capacity - 1; i++)
+    for (unsigned i = 0; i < N - 1; i++)
     {
         _pool[i].next = &_pool[i + 1];
     }
-    _pool[_capacity - 1].next = nullptr;
+    _pool[N - 1].next = nullptr;
 }
 
-template <class T>
-Pool<T>::~Pool()
+template <class T, unsigned N>
+Pool<T, N>::~Pool()
 {
     assert(_rawPool);
     delete[] _rawPool;
 }
 
-template <class T> template <class U, class... Args>
-U *Pool<T>::allocate(Args&& ... args)
+template <class T, unsigned N> template <class U>
+U *Pool<T, N>::allocate()
 {
     static_assert(std::is_base_of<T, U>::value,
                   "can only allocate objects of or subclassed from pool base type");
@@ -74,15 +104,15 @@ U *Pool<T>::allocate(Args&& ... args)
     return object;
 }
 
-template <class T>
-void Pool<T>::release(T *object) noexcept
+template <class T, unsigned N>
+void Pool<T, N>::release(T *object) noexcept
 {
     assert(object);
 
     PoolObject *poolObject = reinterpret_cast<PoolObject *>(object);
 
     assert(poolObject >= _pool);
-    assert(poolObject <= (_pool + sizeof(PoolObject) * (_capacity - 1)));
+    assert(poolObject <= (_pool + sizeof(PoolObject) * (N - 1)));
 
     // Add entry to free list
     PoolObject *oldHead = _freeListHead;
